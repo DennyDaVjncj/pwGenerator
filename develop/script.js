ced0244dfaa1e4424a88d8745e565234e553fead
Denny da Vjncj <daVjncjCode@gmail.com>
2020-09-28 16:49:08 -0400

    initial ideas

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,8 @@
 // Assignment Code
+//button beneath text area, for user to press & gen pw
+let pChar="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()+";
+let lowCase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let upCase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +16,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//create container to store allowed characters
+//prmts/cnfrms/alerts to guid user toward choosing to gen a new PW
