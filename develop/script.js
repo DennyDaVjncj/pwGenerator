81c5c4cea5d2f6de02b27ee7135638e16dfff3e9
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-02 13:30:19 -0400

    writePassword() is doing its job

--- develop/script.js
+++ develop/script.js
@@ -1,71 +1,72 @@
-// Assignment Code
-//button beneath text area, for user to press & gen pw
-// var generateBtn = document.querySelector("#generate");
-// let criterion1=confirm("would you like your PW to include uppercase &lowercase letters?");
-// let criterion2=confirm("would you like to include numbers in PW?");
-// let criterion3=confirm("would you like special characters in PW?");
-// let aboutU=prompt("before receiving your new PW, first tell us something cool about yourself...");
-
-// // Write password to the #password input
 // function writePassword() {
 //   var password=generatePassword();
 //   var passwordText=document.querySelector("#password");
 //   passwordText.value = password;  
 
-// // Add event listener to generate button
+// Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-// Write password to the #password input
-// function talk2user(){    
-//   if(criterion){
-//     alert("your random PW is set to be "+rndmIzer0+" in length");
-//   }  
-// }
-// talk2user();
-
-
 // Assignment Code
-let lowCase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let upCase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let pChar="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()+";
-let rndmIzer=Math.floor(Math.random()*pChar.length);
+let generateBtn=document.querySelector("#generate");
+// let password=generatePassword();//perhaps this method needs defining
+
+let lowCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let nums=[1,2,3,4,5,6,7,8,9,0];
-let nums1=["1234567890".split("")];
-console.log(nums1);
-// let specChar=[*,&,%,$,#]
-let generateBtn = document.querySelector("#generate");
-let usResponse;
+let specChar=["*","&","%","$","#","!"]
+let newArr=[];
+let rndmIzer=Math.floor(Math.random()*newArr.length);
 
+let passwordText=document.querySelector("#password");
+passwordText.value=password;
 
-function writePassword() {
-  let criterion=prompt("Ok, tell us the length of choice for your new, random PW");
+function writePassword(){
+  let criterion=prompt("Ok, your new PW must have at least 8 characters & no more than 128");
   criterion=parseInt(criterion);//this line takes the user 'string' input & turns it to a number dataType
-  // let password = generatePassword();//perhaps this method needs defining
-  // console.log(parseInt(criterion));
-  let passwordText = document.querySelector("#password");
-  passwordText.value = password;
   
-  if(criterion>=8&&criterion<=128){
-    alert("user passed check");
-  }else{
-    alert("not a valid input, must refresh");    
-  };
-  
-  let newArr=[];
-  let criterion0=confirm("would you like to have lowerCase & upperCase letters in your new random PW?");
-  if(criterion0){
-    console.log(newArr);
-    newArr=newArr.concat(upCase);    
-    newArr=newArr.concat(lowCase);  
+
+  if(criterion>=8&&criterion<=128);
+  else{
+    alert("length doesn't meet requirement, we strongly suggest that you do a refresh");
+    return;
   }
 
+  let criterion0=confirm("would you like to have lowerCase letters in your new random PW?");
+  if (criterion0) {
+    newArr=newArr.concat(lowCase);
+    console.log(newArr);
+  } else {
+    alert("ok, we've updated your preference!");
+  }
 
+  let criterion1=confirm("would like to have uppercase letters in your new random PW?")
+  if (criterion1){
+    newArr=newArr.concat(upCase);
+    console.log(newArr);
+  } else {
+    alert("ok, we've updated your preference!");
+  }
+
+  let criterion2=confirm("would like to have numbers in your random PW?");
+  if(criterion2){
+    newArr=newArr.concat(nums);
+    console.log(newArr);
+  }else{
+    alert("ok, we've updated your preference!");
+  }
+
+  let criterion3=confirm("would you like to have special characters in your random PW?");
+  if(criterion3){
+    newArr=newArr.concat(specChar)
+    console.log(newArr);
+  }else{
+    alert("we've updated your preference!");
+  }
 }
 
+function generatePassword(){
+  //this will contain the code to complete said assignment
+}
 
 // Add event listener to generate 
 generateBtn.addEventListener("click", writePassword);
